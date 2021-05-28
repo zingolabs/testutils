c108c285798e4634c1bf6bcd5789ca2ed3812258
zancas <zancas@zingolabs.org>
2021-05-28 11:28:35 -0600

    add Comparator

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::fmt::Display;
+pub struct Comparator<T: PartialEq + Display> {
+    expected: T,
+    observed: T,
+}
+pub impl<T: PartialEq + Display> Comparator<T> {
+    pub fn compare(self) {
+        //! Our convention is that "expected" is "first"
+        if self.expected != self.observed {
+            panic!(
+                "\n===\nExpected:\n{}\nObserved:\n{}\n===\n",
+                self.expected, self.observed
+            );
+        }
     }
 }
